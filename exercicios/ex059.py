b9188118d5ac1748b1eb580553fc042dcd82c8ea
Gesley Peres de Araujo <gesley531@gmail.com>
2021-07-20 11:38:02 -0300

    Class Exercises

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -10,13 +10,14 @@ n2 = int(input('Now enter a number again: '))
 choice = 0
 
 while choice != 5:
+    sleep(1)
     print('\n\033[31mWhat are you doing with these numbers?'
           '\033[33m\n[1]-Want to add?'
           '\n[2]-Do you want to multiply?'
           '\n[3]-The biggest number'
           '\n[4]-New numbers'
           '\n[5]-Finish the program')
-
+    sleep(1)
     choice = int(input('\nEnter your choice: '))
 
     if choice == 1:
@@ -58,6 +59,12 @@ while choice != 5:
         print('\n\033[33mFinishing...')
         sleep(3)
         print('Thanks for using the program, come back often')
+        sleep(1)
+
+    else:
+        sleep(1)
+        print('\033[31mInvalid option, try again...')
+        print('-' * 20)
         sleep(1)
 
 
