8b7ebf531f40c0a5d3bebe26fe1eabe35d68e3cf
Gesley Peres de Araujo <gesley531@gmail.com>
2021-07-20 13:30:33 -0300

    Class Exercises

---
+++ exercicios/ex060.py
@@ -0,0 +1,18 @@
+n = int(input('\n\033[36mEnter a number and I show the factorial value: '))
+c = n
+f = 1
+
+print(f'\033[31mCalculating {n}! = ', end='')
+while c > 0:
+    print(f'\033[33m{c}', end='')
+    if c > 1:
+        print(' x ', end='')
+    else:
+        print(' = ')
+
+    f = f * c
+    c -= 1
+
+print(f'{f}', end='')
+
+# END
